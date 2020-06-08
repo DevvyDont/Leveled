322449478d62c0a882d57bb927ec2494e1712052
Devvy <devinhall4@gmail.com>
2020-06-07 21:07:20 -0400

    refactor how leveled tools work

--- src/main/java/io/github/devvydoo/levelingoverhaul/listeners/progression/PlayerToolListeners.java
+++ src/main/java/io/github/devvydoo/levelingoverhaul/listeners/progression/PlayerToolListeners.java
@@ -6,6 +6,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -17,48 +18,13 @@ import java.util.HashMap;
 public class PlayerToolListeners implements Listener {
 
     LevelingOverhaul plugin;
-    private HashMap<Material, Integer> toolLevelRequirements;
 
     public PlayerToolListeners(LevelingOverhaul plugin) {
-
         this.plugin = plugin;
-
-        toolLevelRequirements = new HashMap<>();
-
-        toolLevelRequirements.put(Material.WOODEN_PICKAXE, 1);
-        toolLevelRequirements.put(Material.WOODEN_HOE, 1);
-        toolLevelRequirements.put(Material.WOODEN_AXE, 1);
-        toolLevelRequirements.put(Material.WOODEN_SHOVEL, 1);
-        toolLevelRequirements.put(Material.WOODEN_SWORD, 1);
-        toolLevelRequirements.put(Material.SHEARS, 1);
-        toolLevelRequirements.put(Material.FISHING_ROD, 1);
-
-        toolLevelRequirements.put(Material.STONE_PICKAXE, LevelRewards.STONE_TOOLS_UNLOCK);
-        toolLevelRequirements.put(Material.STONE_HOE, LevelRewards.STONE_TOOLS_UNLOCK);
-        toolLevelRequirements.put(Material.STONE_AXE, LevelRewards.STONE_TOOLS_UNLOCK);
-        toolLevelRequirements.put(Material.STONE_SHOVEL, LevelRewards.STONE_TOOLS_UNLOCK);
-        toolLevelRequirements.put(Material.STONE_SWORD, LevelRewards.STONE_TOOLS_UNLOCK);
-
-        toolLevelRequirements.put(Material.GOLDEN_PICKAXE, LevelRewards.GOLDEN_TOOLS_UNLOCK);
-        toolLevelRequirements.put(Material.GOLDEN_HOE, LevelRewards.GOLDEN_TOOLS_UNLOCK);
-        toolLevelRequirements.put(Material.GOLDEN_AXE, LevelRewards.GOLDEN_TOOLS_UNLOCK);
-        toolLevelRequirements.put(Material.GOLDEN_SHOVEL, LevelRewards.GOLDEN_TOOLS_UNLOCK);
-        toolLevelRequirements.put(Material.GOLDEN_SWORD, LevelRewards.GOLDEN_TOOLS_UNLOCK);
-
-        toolLevelRequirements.put(Material.IRON_PICKAXE, LevelRewards.IRON_TOOLS_UNLOCK);
-        toolLevelRequirements.put(Material.IRON_HOE, LevelRewards.IRON_TOOLS_UNLOCK);
-        toolLevelRequirements.put(Material.IRON_AXE, LevelRewards.IRON_TOOLS_UNLOCK);
-        toolLevelRequirements.put(Material.IRON_SHOVEL, LevelRewards.IRON_TOOLS_UNLOCK);
-        toolLevelRequirements.put(Material.IRON_SWORD, LevelRewards.IRON_TOOLS_UNLOCK);
-
-        toolLevelRequirements.put(Material.DIAMOND_PICKAXE, LevelRewards.DIAMOND_TOOLS_UNLOCK);
-        toolLevelRequirements.put(Material.DIAMOND_HOE, LevelRewards.DIAMOND_TOOLS_UNLOCK);
-        toolLevelRequirements.put(Material.DIAMOND_AXE, LevelRewards.DIAMOND_TOOLS_UNLOCK);
-        toolLevelRequirements.put(Material.DIAMOND_SHOVEL, LevelRewards.DIAMOND_TOOLS_UNLOCK);
-        toolLevelRequirements.put(Material.DIAMOND_SWORD, LevelRewards.DIAMOND_TOOLS_UNLOCK);
     }
 
-    private void cancelEquipmentUse(Player player, int requiredLevel) {
+    private void cancelEquipmentUse(Cancellable event, Player player, int requiredLevel) {
+        event.setCancelled(true);
         player.sendActionBar( ChatColor.RED + "You must be level " + ChatColor.DARK_RED + requiredLevel + ChatColor.RED + " to use that item!");
         player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_PLACE, .3f, .7f);
     }
@@ -74,16 +40,13 @@ public class PlayerToolListeners implements Listener {
         Player player = event.getPlayer();
         ItemStack toolUsed = player.getInventory().getItemInMainHand();
 
-        if (toolLevelRequirements.containsKey(toolUsed.getType())) {
-            int requiredLevel = plugin.getEnchantmentManager().getItemLevel(toolUsed);
-            if (requiredLevel < toolLevelRequirements.get(toolUsed.getType())) {
-                requiredLevel = toolLevelRequirements.get(toolUsed.getType());
-            }
-            if (requiredLevel > player.getLevel()) {
-                event.setCancelled(true);
-                this.cancelEquipmentUse(player, requiredLevel);
-            }
-        }
+        if (toolUsed.getType() == Material.AIR)
+            return;
+
+        int toolLevel = plugin.getEnchantmentManager().getItemLevel(toolUsed);
+
+        if (toolLevel > player.getLevel())
+            cancelEquipmentUse(event, player, toolLevel);
     }
 
     /**
@@ -94,24 +57,19 @@ public class PlayerToolListeners implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onPlayerAttackEntity(EntityDamageByEntityEvent event) {
 
-        if (!(event.getDamager() instanceof Player)) {
+        if (!(event.getDamager() instanceof Player))
             return;
-        }
 
         Player player = (Player) event.getDamager();
         ItemStack toolUsed = player.getInventory().getItemInMainHand();
 
-        if (toolLevelRequirements.containsKey(toolUsed.getType())) {
-            int requiredLevel = plugin.getEnchantmentManager().getItemLevel(toolUsed);
-            if (requiredLevel < toolLevelRequirements.get(toolUsed.getType())) {
-                requiredLevel = toolLevelRequirements.get(toolUsed.getType());
-            }
-            if (requiredLevel > player.getLevel()) {
-                event.setCancelled(true);
-                this.cancelEquipmentUse(player, requiredLevel);
-            }
-        }
+        if (toolUsed.getType() == Material.AIR)
+            return;
 
+        int toolLevel = plugin.getEnchantmentManager().getItemLevel(toolUsed);
+        
+        if (toolLevel > player.getLevel())
+            cancelEquipmentUse(event, player, toolLevel);
     }
 
 }
